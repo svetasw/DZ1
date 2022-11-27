9f7ad46ea4735f255cf3edc4452c4ee8635f94ad
Sveta <svetasw_17@mail.ru>
2022-11-27 14:57:00 +1100

    DZ8

---
+++ DZ8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
