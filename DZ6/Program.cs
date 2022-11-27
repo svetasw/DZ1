2b1f1a1815fb5fac66702ca3baf21c9fafc0bf8a
Sveta <svetasw_17@mail.ru>
2022-11-27 14:56:30 +1100

    DZ6

---
+++ DZ6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input number : ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Even");
+}
+else
+{
+    Console.WriteLine("Odd");
+}
