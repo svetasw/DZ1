b044c3ac1c42f6c3972405d6f72fa6cbb7902e17
Sveta <svetasw_17@mail.ru>
2022-11-27 14:56:06 +1100

    DZ4

---
+++ DZ4/Program.cs
@@ -0,0 +1,26 @@
+﻿int max = 0;
+
+Console.Write("Input first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine("max = " + max);
+
