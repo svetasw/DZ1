37fbde30c7f260532630cd9db5a11442a49ed48b
Sveta <svetasw_17@mail.ru>
2022-11-27 14:55:40 +1100

    DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,21 @@
+﻿int max = 0;
+int min = 0;
+
+Console.Write("Input first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
+
